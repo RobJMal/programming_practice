5aa15728d975de3aac0e3ef41a683c6c06d5b490
RobJMal <robjmal@gmail.com>
2022-03-21 22:32:50 -0700

    Implemented swapping and assigned directions to numbers

--- permutations_johnson_trotter/johnson_trotter.cpp
+++ permutations_johnson_trotter/johnson_trotter.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <iterator>
+#include <algorithm>
+#include <utility>
 
 // Prints out elements inside vector
 void PrintVector(std::vector<int> vect)
@@ -9,6 +12,37 @@ void PrintVector(std::vector<int> vect)
         std::cout << x << " ";
     }
     std::cout << "" << std::endl;
+}
+
+void JohnsonTrotter(std::vector<int> vect)
+{
+    // Greatest mobile number 
+    int k = * std::max_element(vect.begin(), vect.end());
+
+    std::vector<int> left_pointing_numbers, right_pointing_numbers;
+    left_pointing_numbers = vect;   // Numbers start out as pointing to left 
+
+    PrintVector(vect);  // initilized vector is permutation 
+
+
+    // Finding index of k 
+    std::vector<int>::iterator itr = std::find(vect.begin(), vect.end(), k);
+    int k_index = std::distance(vect.begin(), itr);
+
+    if (std::find(left_pointing_numbers.begin(), left_pointing_numbers.end(), k) != left_pointing_numbers.end())
+    {
+        // Swap to the left 
+        std::swap(vect[k_index - 1], vect[k_index]);
+    }
+    else if (std::find(right_pointing_numbers.begin(), right_pointing_numbers.end(), k) != right_pointing_numbers.end())
+    {
+        // Swap to the right 
+        std::swap(vect[k_index], vect[k_index + 1]);
+    }
+    
+    PrintVector(vect);
+
+
 }
 
 int main()
@@ -22,7 +56,7 @@ int main()
         perm_vect.push_back(i + 1);
     }
 
-    
+    JohnsonTrotter(perm_vect);
     
     return 0;
 }
